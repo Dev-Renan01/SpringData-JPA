7c668d3f895d18d5959fec9b94cb0e3137b1d4c3
Thiago Renan <thiagorenan1108@gmail.com>
2025-09-02 13:38:41 -0300

    Projeto criado

---
+++ projeto-spring-data/src/main/java/projeto/spring/data/projeto/spring/data/App.java
@@ -0,0 +1,7 @@
+package projeto.spring.data.projeto.spring.data;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ projeto-spring-data/src/test/java/projeto/spring/data/projeto/spring/data/AppTest.java
@@ -0,0 +1,10 @@
+package projeto.spring.data.projeto.spring.data;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+  }
+}
