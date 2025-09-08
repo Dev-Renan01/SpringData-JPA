c60b7fc6528f00eff3299a64d8a035df954c8cd4
Thiago Renan <thiagorenan1108@gmail.com>
2025-09-08 18:26:35 -0300

    Criada a classe de teste

---
+++ projeto-spring-data/src/test/java/projeto/spring/data/projeto/spring/data/SpringDataTest.java
@@ -0,0 +1,16 @@
+package projeto.spring.data.projeto.spring.data;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)//Integra os testes JUnit 5 com o Spring
+@ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})// Indica ao Spring qual arquivo XML de configuração carregar
+public class SpringDataTest {
+
+	@Test
+	public void testInsert() {
+		System.out.println("Iniciou Spring com sucesso");
+	}
+}
