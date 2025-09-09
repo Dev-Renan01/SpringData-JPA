dc30b18a56e85cec693681b739572793f04d0b62
Thiago Renan <thiagorenan1108@gmail.com>
2025-09-09 13:55:57 -0300

    DAO Repository

---
+++ projeto-spring-data/src/main/java/dao/InterfaceSpringDataUser.java
@@ -0,0 +1,11 @@
+package dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import model.UsuarioSpringData;
+
+@Repository
+public interface InterfaceSpringDataUser extends CrudRepository<UsuarioSpringData, Long> {
+
+}
